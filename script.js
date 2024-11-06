e073a7e85845899b12b7e75312b542e810cc6855
Glitch (fringe-various-base) <none>
2024-11-06 17:23:54 +0000

    🗑️🔓 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgCwODUtMxUbNeqDz54m2P038Fkh_IMOE",
+  authDomain: "md-tictactoe.firebaseapp.com",
+  projectId: "md-tictactoe",
+  storageBucket: "md-tictactoe.firebasestorage.app",
+  messagingSenderId: "891613065066",
+  appId: "1:891613065066:web:3ec62e91250db004b8ff13"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
